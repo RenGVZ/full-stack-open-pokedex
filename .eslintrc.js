517b5691c0de50b27e654d8b350a32caf9c7dff5
Gavin Garza <gaving@live.com>
2023-10-10 15:34:40 +0900

    pipeline test 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,11 @@ module.exports = {
       "error", { "before": true, "after": true }
     ],
     "no-console": "error",
-    "react/prop-types": 0
+    "react/prop-types": 0,
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
   }
 }
