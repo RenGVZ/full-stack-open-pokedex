3faa2b3b2c5dd277c89bdbf74828f5a10518a478
Gavin Garza <gaving@live.com>
2023-10-12 20:59:35 +0900

    render deploy health check 3

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 4000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw new Error('oh no')
-  // eslint-disable-next-line no-unreachable
   res.send('healthy yo')
 })
 
